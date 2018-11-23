c9b979af85559264186ceb3ccf8265b5a192c9ac
sapuri <info@minami.xyz>
2018-11-23 19:44:53 +0900

    fix: fix typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1 maximum-scale=1, minimum-scale=1, user-scalable=no'
+          'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no'
       },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
